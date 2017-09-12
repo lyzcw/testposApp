63626db0fc37b5256766c6e0b463791e29d4df09
lyzcw <lyzcw@sina.com>
2017-09-12 13:41:56 +0800

    抽取打aar包前提交

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,16 +13,11 @@ export class HomePage {
   @ViewChild('response1') response1: any;
 
   //private events: Events,
-  constructor(private platform: Platform, public navCtrl: NavController, public renderer: Renderer) {
+  constructor(private platform: Platform, private events: Events, public navCtrl: NavController, public renderer: Renderer) {
     platform.ready().then((e) => {
-      // window.addEventListener('readcard', function(e) {
-      //   alert('有刷卡' + e);
-      //   console.log('有刷卡');
-      //   //let temp = this.homeModel.response;
-      //   //this.homeModel.response = temp + "\r\n*******有刷卡事件*******\r\n" + "Event:" + e;// + "Info:" + data;
-      // }, false );
-      //window.addEventListener('readcard', this.onReadCard, false );
-
+      
+      window.addEventListener('readcard', this.onReadCard, false );
+      window.addEventListener('scancode', this.onScanCode, false );
       //this.events.subscribe('readcard', () => this.onReadCard( e ));
       
     });
@@ -32,38 +27,25 @@ export class HomePage {
   homeModel: HomeModel = {"response":"等待POS响应中..."};
   
   onReadCard( data ): void{
-    //alert('有刷卡：' + data.info);
+    alert('刷卡信息：' + data.info);
     console.log('有刷卡：' + data.info );
     let temp = this.homeModel.response;
-    this.homeModel.response = "\r\n*******有刷卡事件*******\r\n" + "Event:" + data.info; // + "Info:" + data;
+    this.homeModel.response = "\r\n*******有刷卡事件*******\r\n" + data.info; // + "Info:" + data;
+  }
+
+  onScanCode( data ): void{
+    alert('扫码信息：' + data.info);
+    console.log('扫码：' + data.info );
+    let temp = this.homeModel.response;
+    this.homeModel.response = "\r\n*******有扫码事件*******\r\n" + data.info; // + "Info:" + data;
   }
 
   openCardReader(): void {
     
     let promise = new Promise((resolve, reject) => {
       cordova.plugins.nlpos.openCardReader( data => {
-        let temp = this.homeModel.response;
-        this.homeModel.response = data; //temp + "\r\n******************************************\r\n" + data;
-        // this.renderer.setElementStyle(this.response1.nativeElement, 'scrollTop', this.response1.nativeElement.clientHeight + 'px');
-        let timeout=240;
-        const interval =setInterval(() => {
-          console.log('取异步数据：' + timeout + ":次");
-          if( timeout == 0){
-            clearInterval(interval);
-          }else{
-            this.getAsynMsg().then(data => {
-              console.log('取到异步数据2：' + data );
-              if( ""!=data && data != undefined && null != data){
-                this.homeModel.response = data;
-                clearInterval(interval);
-                resolve(data);
-              }
-            });
-            
-          }
-          timeout--;
-        }, 500);
-        
+        this.homeModel.response = data;
+        resolve(data);
       }, error => {
         reject(error);
       });
@@ -87,8 +69,7 @@ export class HomePage {
     
     let promise = new Promise((resolve, reject) => {
       cordova.plugins.nlpos.closeCardReader( data => {
-        let temp = this.homeModel.response;
-        this.homeModel.response = data; //temp + "\r\n******************************************\r\n" + data;
+        this.homeModel.response = data;
         this.renderer.setElementStyle(this.response1.nativeElement, 'scrollTop', this.response1.nativeElement.clientHeight + 'px');
         resolve(data);
       }, error => {
@@ -101,27 +82,8 @@ export class HomePage {
   scan(): void {
     let promise = new Promise((resolve, reject) => {
       cordova.plugins.nlpos.scan( data => {
-        let temp = this.homeModel.response;
-        this.homeModel.response = data; //temp + "\r\n******************************************\r\n" + data;
-        let timeout=20;
-        const interval0 =setInterval(() => {
-          console.log('取扫码异步数据：' + timeout + ":次");
-          if( timeout == 0){
-            clearInterval(interval0);
-          }else{
-            this.getAsynMsg().then(data => {
-              console.log('取到扫码异步数据2：' + data );
-              if( ""!=data && data != undefined && null != data){
-                this.homeModel.response = data;
-                clearInterval(interval0);
-                resolve(data);
-              }
-            });
-            
-          }
-          timeout--;
-        }, 500);
-
+        this.homeModel.response = data;
+        resolve(data);
       }, error => {
         reject(error);
       });
@@ -136,7 +98,6 @@ export class HomePage {
       bill += "-----------------------------\n";
       bill += "+++++++++++++++++++++++++++++\n";
       cordova.plugins.nlpos.print((bill), data => {
-        let temp = this.homeModel.response;
         this.homeModel.response = data; //temp + "\r\n******************************************\r\n" + data;
         resolve(data);
       }, error => {
